daa148088d899a5e8a8a396f8059da0a79482ad0
ximenatiscareno08 <ximena.tiscareno.22e@utzmg.edu.mx>
2024-03-14 11:03:26 -0600

    primera versión

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.listen(3000, ()=> {
+    console.log("conectado a puerto 3000");
+});
